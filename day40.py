455015134f1e28dfe2df202e0a49fcfbfe3d90e0
Atharva Degwekar <72964751+atharvahacks@users.noreply.github.com>
2025-03-03 16:42:20 +0530

    Add files via upload

---
+++ day40.py
@@ -0,0 +1,47 @@
+# Day40
+# Exercise 4
+# Secret Code
+# Write a python program to translate a message into secret code language. Use the rules below to trasnlate the message into secret code.
+# Coding:
+# if the word contains atleast 3 characters, remove the first letter and append it at the end
+#   now append three random characters at the starting and the end
+# else:
+#   simply reverse the string
+
+# Decoding:
+# if the word contains less than 3 characters, reverse it
+# else:
+#   remove 3 random characters from start and end. Now remove the last letter and append it to the beginning
+
+# Your program should ask whether you want to code or decode the message. Then it should ask for the message and print the coded/decoded message.
+
+# coding:
+# Choice weather the user wants to decode or encode the message
+
+print(f"Enter the choice you want to go with: \n1. Encode \n2. Decode")
+choice = int(input())
+
+# Encoding the message
+def encode(message):
+    if len(message) >= 3:
+        message = message[1:] + message[0]
+        message = 'abc' + message + 'xyz'
+    else:
+        message = message[::-1]
+    return message
+
+# Decoding the message
+def decode(message):
+    if len(message) < 3:
+        message = message[::-1]
+    else:
+        message = message[3:-3]
+        message = message[-1] + message[:-1]
+    return message
+
+if choice == 1:
+    message = input("Enter the message you want to encode: ")
+    print(f"The encoded message is: {encode(message)}")
+else:
+    message = input("Enter the message you want to decode: ")
+    print(f"The decoded message is: {decode(message)}")
---
+++ day41.py
@@ -0,0 +1,10 @@
+# Day 41
+# Short hand if else
+# This is a short hand if else statement. It is used to write a single line if else statement. It is used when there is only one statement to be executed in the if or else block. It is written as:
+# statement if condition else statement
+# It is used to check a condition and execute a statement if the condition is true. If the
+# condition is false, the statement after the else keyword is executed.
+A = 2
+B = 330
+print(A) if A > B else print(f"{B} is greater than {A}")
+print(B) if A > B else print(f"{A} is lesser than {B}")
